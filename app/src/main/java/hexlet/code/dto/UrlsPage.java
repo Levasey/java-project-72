67f51d7b001457a83a390468c6cb2d29e67d7d62
Your Name <you@example.com>
2025-10-08 20:31:28 +0600

    Add RootController, NamedRoutes, FlashUtil. Update App

--- app/src/main/java/hexlet/code/dto/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/UrlsPage.java
@@ -2,30 +2,18 @@ package hexlet.code.dto;
 
 import hexlet.code.model.Url;
 import hexlet.code.model.UrlCheck;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @Accessors(chain = true)
 public final class UrlsPage extends BasePage {
     private List<Url> urls;
     private Map<Long, UrlCheck> latestChecks = new HashMap<>();
-
-    public List<Url> getUrls() {
-        return urls;
-    }
-
-    public void setUrls(List<Url> urls) {
-        this.urls = urls;
-    }
-
-    public Map<Long, UrlCheck> getLatestChecks() {
-        return latestChecks;
-    }
-
-    public void setLatestChecks(Map<Long, UrlCheck> latestChecks) {
-        this.latestChecks = latestChecks;
-    }
 }
--- app/src/main/java/hexlet/code/model/UrlCheck.java
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -1,8 +1,13 @@
 package hexlet.code.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Setter
+@Getter
 public final class UrlCheck {
     private Long id;
     private int statusCode;
@@ -22,61 +27,5 @@ public final class UrlCheck {
 
     public UrlCheck() {
         this.createdAt = LocalDateTime.now();
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getH1() {
-        return h1;
-    }
-
-    public void setH1(String h1) {
-        this.h1 = h1;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public long getUrlId() {
-        return urlId;
-    }
-
-    public void setUrlId(long urlId) {
-        this.urlId = urlId;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
     }
 }
